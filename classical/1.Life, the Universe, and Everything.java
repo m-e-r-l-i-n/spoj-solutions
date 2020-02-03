c71751992931e661ae9905df322f0596589ecd83
Tamonash Chakraborty <59210880+tamo14@users.noreply.github.com>
2020-02-03 16:04:49 +0530

    TEST
    problem link-https://www.spoj.com/problems/TEST/

---
+++ classical/1.Life, the Universe, and Everything.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws Exception
+	{
+		BufferedReader bu=new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<Integer> l=new ArrayList<>();
+		String s=bu.readLine();
+		while(s!=null)
+		{if(Integer.parseInt(s)==42)
+		break;
+		else
+		l.add(Integer.parseInt(s));
+		s=bu.readLine();}
+		for(int i=0;i<l.size();i++)
+		System.out.println(l.get(i));		
+	}
+}
