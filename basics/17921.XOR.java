6a5b764267d980f34c549a577e8ba690b042b90c
Tamonash Chakraborty <59210880+tamo14@users.noreply.github.com>
2020-02-03 16:06:47 +0530

    BSCXOR
    problem link-https://www.spoj.com/problems/BSCXOR/

---
+++ basics/17921.XOR.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int p=sc.nextInt(), q=sc.nextInt();
+		System.out.print(p^q);
+	}
+}
